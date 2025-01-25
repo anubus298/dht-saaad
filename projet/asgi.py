3c7f3ffed9cc264a38a8699b3e22c0d5999de662
anubus298 <araristaf@gmail.com>
2025-01-25 01:17:16 +0100

    migrating to postgres

--- projet/asgi.py
+++ projet/asgi.py
@@ -14,3 +14,9 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projet.settings')
 
 application = get_asgi_application()
+app = application
+
+
+
+
+
